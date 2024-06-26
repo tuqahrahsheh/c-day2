ca8d854946d0cc9d6198ddd7fd4d0d5a6d36de2d
tuqahrahsheh <169064779+tuqahrahsheh@users.noreply.github.com>
2024-06-26 23:42:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxk2__26_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+            Console.WriteLine("1");
+            int number1 = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("3");
+            int number2 = int.Parse(Console.ReadLine());
+            int smallerNumber = Math.Min(number1, number2);
+
+             Console.WriteLine();
+
+
+        }
+    }
+}
